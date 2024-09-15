2442eaf45b8bc920f0c5458b66ed2d84bd094a6a
Nithin Prakash <nithinpnandanam@gmail.com>
2024-09-15 15:58:16 +0530

    complete suggestion box

--- src/utils/consatnts.jsx
+++ src/utils/consatnts.jsx
@@ -1,3 +1,4 @@
 const YT_API_KEY = import.meta.env.VITE_YT_API_KEY
 export const allVideos =  `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${YT_API_KEY}`
+export const searchAPI = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
 
