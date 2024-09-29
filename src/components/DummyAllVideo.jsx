a15040c86f036c1fc33a5ede40bcf16d0eaf6618
Nithin Prakash <nithinpnandanam@gmail.com>
2024-09-29 22:09:33 +0530

    add shimmer ui and chanded video count to a areadable format

---
+++ src/components/DummyAllVideo.jsx
@@ -0,0 +1,38 @@
+
+
+ const DummyAllVideo = () => {
+  return (
+    <div className="h-screen flex flex-wrap bg-black ">
+        <div className="w-[345px] h-[240px] bg-customGray2 m-4 ">
+        </div>
+        <div className="w-[345px] h-[240px] bg-customGray2 m-4 ">
+        </div>
+        <div className="w-[345px] h-[240px] bg-customGray2 m-4  ">
+        </div>
+        <div className="w-[345px] h-[240px] bg-customGray2 m-4 ">
+        </div>
+        <div className="w-[345px] h-[240px] bg-customGray2 m-4 ">
+        </div>
+        <div className="w-[345px] h-[240px] bg-customGray2 m-4 ">
+        </div>
+        <div className="w-[345px] h-[240px] bg-customGray2 m-4 ">
+        </div>
+        <div className="w-[345px] h-[240px] bg-customGray2 m-4 ">
+        </div>
+        <div className="w-[345px] h-[240px] bg-customGray2 m-4 ">
+        </div>
+        <div className="w-[345px] h-[240px] bg-customGray2 m-4 ">
+        </div>
+        <div className="w-[345px] h-[240px] bg-customGray2 m-4 ">
+        </div>
+        <div className="w-[345px] h-[240px] bg-customGray2 m-4 ">
+        </div>
+        <div className="w-[345px] h-[240px] bg-customGray2 m-4 ">
+        </div>
+        <div className="w-[345px] h-[240px] bg-customGray2 m-4 ">
+        </div>
+    </div>
+  )
+}
+
+export default DummyAllVideo
